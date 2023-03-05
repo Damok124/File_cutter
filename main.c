9b95508e5c60d5f4e4f642eef95e5f3cb3214f0b
Damok124VM <zharzi@student.42angouleme.fr>
2023-03-05 23:00:18 +0100

    05mar23

--- main.c
+++ main.c
@@ -6,24 +6,11 @@
 /*   By: zharzi <zharzi@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 11:30:42 by zharzi            #+#    #+#             */
-/*   Updated: 2023/02/06 17:10:37 by zharzi           ###   ########.fr       */
+/*   Updated: 2023/03/05 22:58:53 by zharzi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "file_cutter.h"
-
-//prend en parametre 1 le nom du fichier a decouper avec check .c				done
-//doit lire un fichier,
-//doit detecter l'include et le strdup
-//doit stocker les char *suivant l'include jusqu'a l'acc fermante dans un char **
-//lorsqu'il detecte l'accolade fermante il recupere le nom de la fonction et join .c
-//stock le nom de toutes les fonctions
-//check si un nom du lot existe deja dans le dossier et exit si oui
-//si RAS, relire fichier
-//lorsqu'il detecte le nom de l'include, il ouvre un fichier au nom prevu,
-//puis il putstr fd dans le fichier jusqu'a detectection du prochain include
-//doit close le fichier
-//
 
 int	main(int ac, char **argv)
 {
